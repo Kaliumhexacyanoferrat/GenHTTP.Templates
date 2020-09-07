c1c585523578a8174d9a5b5dbc5ef8ea1d76e1ff
Andreas Nägeli <andr.naegeli@gmail.com>
2020-09-07 14:38:02 +0200

    Add template for vue.js app with API

---
+++ Templates/vue-api/App/src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  
+  <div class="main">
+
+    <div v-for="task in tasks">
+        {{ task }}
+    </div>
+    
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:8080/api/tasks')
+      .then(response => response.json())
+      .then(data => this.tasks = data)
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
