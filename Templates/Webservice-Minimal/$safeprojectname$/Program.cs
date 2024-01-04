74dddd95616b0c2675971e52e299141d7f8ead8c
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-01-04 17:19:43 +0100

    Add test project to minimal webservice template

--- Templates/Webservice-Minimal/$safeprojectname$/Program.cs
+++ Templates/Webservice-Minimal/$safeprojectname$/Program.cs
@@ -1,29 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using GenHTTP.Engine;
 
-using GenHTTP.Engine;
-
-using GenHTTP.Modules.Functional;
-using GenHTTP.Modules.Layouting;
 using GenHTTP.Modules.Practices;
-using GenHTTP.Modules.Security;
 
-// run this project and fetch a list of book records via http://localhost:8080/books/
-var books = new List<Book>()
-{
-  new Book(1, "Lord of the Rings")
-};
+using $safeprojectname$;
 
-// see https://genhttp.org/documentation/content/functional
-var bookApi = Inline.Create()
-                    .Get(() => books)
-                    .Put((Book book) => books.Add(book));
-
-var content = Layout.Create()
-                    .Add("books", bookApi)
-                    .Add(CorsPolicy.Permissive());
+var project = Project.Setup();
 
 return Host.Create()
-           .Handler(content)
+           .Handler(project)
            .Defaults()
            .Console()
 //-:cnd:noEmit
@@ -32,5 +16,3 @@ return Host.Create()
 #endif
 //+:cnd:noEmit
            .Run();
-
-record class Book(int ID, string Title);
