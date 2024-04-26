694fa48a366c9fc0c0b442bf8caa00b7952cb2ab
Andreas Nägeli <andr.naegeli@gmail.com>
2024-04-26 14:32:47 +0200

    Add template for Single Page Applications

---
+++ Templates/SinglePageApplication/$safeprojectname$.Tests/ApplicationTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GenHTTP.Testing;
+
+namespace $safeprojectname$.Tests
+{
+
+    [TestClass]
+    public class ApplicationTests
+    {
+
+        [TestMethod]
+        public async Task TestRoot()
+        {
+            using var runner = TestHost.Run(Project.Setup());
+
+            using var response = await runner.GetResponseAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+
+}
