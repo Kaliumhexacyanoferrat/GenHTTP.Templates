32b85a674cb1f3e2a6c45c775a48814fe9765690
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-23 11:36:39 +0100

    Add Swagger UI to service frameworks

--- Templates/Webservice-Controllers/$safeprojectname$/Program.cs
+++ Templates/Webservice-Controllers/$safeprojectname$/Program.cs
@@ -4,6 +4,24 @@ using GenHTTP.Modules.Practices;
 
 using $safeprojectname$;
 
+//
+// GenHTTP Controller Framework Template
+//
+// URLs:
+//   http://localhost:8080/devices/
+//   http://localhost:8080/docs/
+//   http://localhost:8080/openapi.json
+//
+// Framework documentation:
+//   https://genhttp.org/documentation/content/frameworks/controllers/
+//
+// Method definitions:
+//   https://genhttp.org/documentation/content/concepts/definitions/
+//
+// Additional features:
+//   https://genhttp.org/documentation/content/
+//
+
 var project = Project.Setup();
 
 return await Host.Create()
--- Templates/Webservice/$safeprojectname$/Project.cs
+++ Templates/Webservice/$safeprojectname$/Project.cs
@@ -1,9 +1,11 @@
-﻿using GenHTTP.Api.Content;
+﻿using System;
+using GenHTTP.Api.Content;
 
 using GenHTTP.Modules.Layouting;
 using GenHTTP.Modules.Security;
 using GenHTTP.Modules.Webservices;
 using GenHTTP.Modules.OpenApi;
+using GenHTTP.Modules.ApiBrowsing;
 
 using $safeprojectname$.Services;
 
@@ -17,6 +19,7 @@ public static class Project
         return Layout.Create()
                      .AddService<BookService>("books")
                      .AddOpenApi()
+                     .AddSwaggerUI(segment: "docs")
                      .Add(CorsPolicy.Permissive());
     }
 
