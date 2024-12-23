7ad6a8d115c2fd3d70222550f0168cae0069b898
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-23 13:59:54 +0100

    Add template for Server Sent Events (SSE) (#19)

---
+++ Templates/ServerSentEvents/$safeprojectname$.Tests/EventStreamTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GenHTTP.Testing;
+
+namespace $safeprojectname$.Tests;
+
+[TestClass]
+public class EventStreamTests
+{
+
+    [TestMethod]
+    public async Task TestIndexServed()
+    {
+        await using var runner = await TestHost.RunAsync(Project.Setup());
+
+        using var response = await runner.GetResponseAsync();
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+}
---
+++ Templates/ServerSentEvents/$safeprojectname$/Program.cs
@@ -0,0 +1,31 @@
+﻿using GenHTTP.Engine.Internal;
+
+using GenHTTP.Modules.Practices;
+
+using $safeprojectname$;
+
+//
+// GenHTTP Server Sent Events Template
+//
+// URLs:
+//   http://localhost:8080/
+//
+// Handler documentation:
+//   https://genhttp.org/documentation/content/handlers/server-sent-events/
+//
+// Additional features:
+//   https://genhttp.org/documentation/content/
+//
+
+var project = Project.Setup();
+
+return await Host.Create()
+                 .Handler(project)
+                 .Defaults()
+                 .Console()
+//-:cnd:noEmit
+#if DEBUG
+                 .Development()
+#endif
+//+:cnd:noEmit
+                 .RunAsync();
